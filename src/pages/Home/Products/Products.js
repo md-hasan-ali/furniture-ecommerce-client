a274d6c858ff9436551bef5a42d20a546f0c2521
md-hasan-ali <hasanaliringku@gmail.com>
2022-01-05 00:45:29 +0600

    create products component

--- src/pages/Home/Products/Products.js
+++ src/pages/Home/Products/Products.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react';
+import Rating from 'react-rating';
 import './Products.css';
 
 const Products = () => {
@@ -11,8 +12,32 @@ const Products = () => {
     console.log(products)
 
     return (
-        <div>
-
+        <div className='products-area my-5'>
+            <div className="container">
+                <div className="section-title text-center">
+                    <h2 className='pb-5'>Main <span className='text-style'>Products</span></h2>
+                </div>
+                <div className="row">
+                    {
+                        products.map(product =>
+                            <div key={product._id} className='col-md-4'>
+                                <div className="single-product text-center">
+                                    <img src={product.img} alt="" />
+                                    <h4 className='m-0'>{product.name}</h4>
+                                    <strong><p>{product.price}</p></strong>
+                                    <p><Rating
+                                        style={{ color: "#315160", paddingBottom: "10px", }}
+                                        initialRating={product.start}
+                                        emptySymbol="far fa-star"
+                                        fullSymbol="fas fa-star"
+                                        readonly
+                                    /> <strong style={{ color: "#000" }}>{product.start}</strong> </p>
+                                </div>
+                            </div>
+                        )
+                    }
+                </div>
+            </div>
         </div>
     );
 };
