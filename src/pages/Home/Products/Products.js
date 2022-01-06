2148f36ae0eff974a1bfd20d359f643bca98df04
md-hasan-ali <hasanaliringku@gmail.com>
2022-01-06 12:18:30 +0600

    add heroku link

--- src/pages/Home/Products/Products.js
+++ src/pages/Home/Products/Products.js
@@ -1,11 +1,12 @@
 import React, { useEffect, useState } from 'react';
+import { Spinner } from 'react-bootstrap';
 import Rating from 'react-rating';
 import './Products.css';
 
 const Products = () => {
     const [products, setProducts] = useState([]);
     useEffect(() => {
-        fetch('http://localhost:4000/products')
+        fetch('https://still-thicket-39969.herokuapp.com/products')
             .then(res => res.json())
             .then(data => setProducts(data))
     }, [])
@@ -17,31 +18,37 @@ const Products = () => {
                 <div className="section-title text-center">
                     <h2 className='pb-5'>Main <span className='text-style'>Products</span></h2>
                 </div>
-                <div className="row">
-                    {
-                        products.map(product =>
-                            <div key={product._id} className='col-md-4'>
-                                <div className="single-product text-center">
-                                    <img src={product.img} alt="" />
-                                    <h4 className='m-0'>{product.name}</h4>
-                                    <strong><p>{product.price}</p></strong>
-                                    <p className='rating'><Rating
-                                        style={{ color: "#315160", paddingBottom: "10px", }}
-                                        initialRating={product.start}
-                                        emptySymbol="far fa-star"
-                                        fullSymbol="fas fa-star"
-                                        readonly
-                                    /> <strong style={{ color: "#315160" }}>({product.start})</strong> </p>
-                                    <div className="overlay pb-3">
-                                        <button className='btn btn-success text-white'>Add To Cart <i class="fas fa-cart-plus"></i></button>
+                {
+                    products.length === 0 ?
+                        <div className="text-center">
+                            <Spinner size="xl" animation="border" variant="danger" />
+                        </div>
+                        : <div className="row">
+                            {
+                                products.map(product =>
+                                    <div key={product._id} className='col-md-4'>
+                                        <div className="single-product text-center">
+                                            <img src={product.img} alt="" />
+                                            <h4 className='m-0'>{product.name}</h4>
+                                            <strong><p>{product.price}</p></strong>
+                                            <p className='rating'><Rating
+                                                style={{ color: "#315160", paddingBottom: "10px", }}
+                                                initialRating={product.start}
+                                                emptySymbol="far fa-star"
+                                                fullSymbol="fas fa-star"
+                                                readonly
+                                            /> <strong style={{ color: "#315160" }}>({product.start})</strong> </p>
+                                            <div className="overlay pb-3">
+                                                <button className='btn btn-success text-white'>Add To Cart <i class="fas fa-cart-plus"></i></button>
+                                            </div>
+                                        </div>
                                     </div>
-                                </div>
-                            </div>
-                        )
-                    }
-                </div>
+                                )
+                            }
+                        </div>
+                }
             </div>
-        </div>
+        </div >
     );
 };
 
