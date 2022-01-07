25a302f0dd319dd5d62c96a35879be523cc8ceff
md-hasan-ali <hasanaliringku@gmail.com>
2022-01-08 00:22:41 +0600

    create all products pages

--- src/pages/AboutUs/About.js
+++ src/pages/AboutUs/About.js
@@ -1,7 +1,11 @@
 // Import Necessary file 
 import React from 'react';
+import { Col, Container, Row } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
 import Footer from '../Sheard/Footer/Footer';
 import Navigarion from '../Sheard/Navigation/Navigarion';
+import aboutImg from '../../images/hasanali.jpg'
+import './About.css'
 
 // About functional Component 
 const About = () => {
@@ -9,15 +13,25 @@ const About = () => {
         <>
             <Navigarion></Navigarion>
 
-            <div className="about-area">
-                <div className="container">
-                    <div className="row">
-                        <div className="col-md-6">
-                            <h2>This is About area..</h2>
+            <Container className='my-5'>
+                <h2 className='text-center pb-4'>About <span className='text-highlight'>Us</span></h2>
+                <Row className='align-items-center'>
+                    <Col xs={12} md={6}>
+                        <div className="about-content">
+                            <h2><span className='text-highlight'>About</span> Me!!!</h2>
+                            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Cum porro quos atque, excepturi fugit, error sed, rem nihil doloremque laboriosam accusamus reiciendis laudantium eaque qui ut dolores at! Accusamus quam ullam dolor voluptatibus possimus odio, ex ipsam, unde minima et, adipisci ut. Reprehenderit ut quod illo expedita corporis voluptate aperiam.</p>
+                            <Link to='/contact'>
+                                <button className='btn btn-success box-button'>Contact Me!!</button>
+                            </Link>
                         </div>
-                    </div>
-                </div>
-            </div>
+                    </Col>
+                    <Col xs={12} md={6}>
+                        <div className="about-image text-center">
+                            <img src={aboutImg} alt="" />
+                        </div>
+                    </Col>
+                </Row>
+            </Container>
 
             <Footer></Footer>
         </>
--- src/pages/AllProducts/AllProduct.js
+++ src/pages/AllProducts/AllProduct.js
@@ -1,21 +1,65 @@
 // import necessary file 
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { Spinner } from 'react-bootstrap';
+import Rating from 'react-rating';
+import { Link } from 'react-router-dom';
 import Footer from '../Sheard/Footer/Footer';
 import Navigarion from '../Sheard/Navigation/Navigarion';
 
 // All Products Component 
 const AllProduct = () => {
+    const [products, setProducts] = useState([]);
+
+    useEffect(() => {
+        fetch('https://still-thicket-39969.herokuapp.com/products')
+            .then(res => res.json())
+            .then(data => setProducts(data))
+    }, [])
+    console.log(products)
+
     return (
         <>
             <Navigarion></Navigarion>
 
-            <div className="product-area">
+            <div className='products-area my-5'>
                 <div className="container">
-                    <div className="row">
-                        <h2>This is products Area..</h2>
+                    <div className="section-title text-center">
+                        <h2 className='pb-5'>All <span className='text-style'>Products</span></h2>
                     </div>
+                    {
+                        products.length === 0 ?
+                            <div className="text-center">
+                                <Spinner size="xl" animation="border" variant="danger" />
+                            </div>
+                            : <div className="row">
+                                {
+                                    products.map(product =>
+                                        <div key={product._id} className='col-md-4'>
+                                            <div className="single-product text-center">
+                                                <img src={product.img} alt="" />
+                                                <h4 className='m-0'>{product.name}</h4>
+                                                <strong><p>{product.price}</p></strong>
+                                                <p className='rating'><Rating
+                                                    style={{ color: "#315160", paddingBottom: "10px", }}
+                                                    initialRating={product.start}
+                                                    emptySymbol="far fa-star"
+                                                    fullSymbol="fas fa-star"
+                                                    readonly
+                                                /> <strong style={{ color: "#315160" }}>({product.start})</strong> </p>
+                                                <div className="overlay pb-3">
+
+                                                    <Link to={`/productsDetail/${product?._id}`}>
+                                                        <button className='btn btn-success text-white'>Add To Cart <i class="fas fa-cart-plus"></i></button>
+                                                    </Link>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    )
+                                }
+                            </div>
+                    }
                 </div>
-            </div>
+            </div >
 
             <Footer></Footer>
         </>
